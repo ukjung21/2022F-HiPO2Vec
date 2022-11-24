79865d968f17098c5a3ccb69f8980f36428f2c88
ukjung21 <80200554+ukjung21@users.noreply.github.com>
2022-11-25 01:49:58 +0900

    Update HPOfull_embedding.sh

--- HPOfull_embedding.sh
+++ HPOfull_embedding.sh
@@ -1,6 +1,6 @@
 python3 embedGO.py \
-       -dim 500 \
-       -lr 0.03 \
+       -dim 100 \
+       -lr 0.12 \
        -epochs 1000 \
        -negs 50 \
        -batchsize 50 \
@@ -8,48 +8,6 @@ python3 embedGO.py \
        -gpu 0 \
        -manifold poincare \
        -dset data/HPOfull_train.tsv \
-       -checkpoint result/HPOfull_d500.pth \
+       -checkpoint result/HPOfull_d100_lr40.pth \
        -fresh \
        -sparse
-
-python3 embedGO.py \
-       -dim 1000 \
-       -lr 0.015 \
-       -epochs 1000 \
-       -negs 50 \
-       -batchsize 50 \
-       -burnin 20 \
-       -gpu 0 \
-       -manifold poincare \
-       -dset data/HPOfull_train.tsv \
-       -checkpoint result/HPOfull_d1000.pth \
-       -fresh \
-       -sparse
-       
-#python3 embedGO.py \
-#       -dim 200 \
-#       -lr 0.12 \
-#       -epochs 500 \
-#       -negs 50 \
-#       -batchsize 50 \
-#       -burnin 20 \
-#       -gpu 0 \
-#       -manifold poincare \
-#       -dset data/HPOfull_train_con.tsv \
-#       -checkpoint result/conHPOfull_example_d200_lr40_e500.pth \
-#       -fresh \
-#       -sparse
-       
-# python3 embedGO.py \
-#        -dim 100 \
-#        -lr 0.24 \
-#        -epochs 1000 \
-#        -negs 50 \
-#        -batchsize 20 \
-#        -burnin 20 \
-#        -gpu 0 \
-#        -manifold poincare \
-#        -dset data/HPOfull_train.tsv \
-#        -checkpoint result/conHPOfull_d100_lr80_b20.pth \
-#        -fresh \
-#        -sparse
